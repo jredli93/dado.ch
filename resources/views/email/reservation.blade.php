75abb0aff1ffebd041ba48d7f83d1ed51557cb07
Cajox <j.miljkovic95@gmail.com>
2021-02-21 13:44:41 +0100

    fix

--- resources/views/email/reservation.blade.php
+++ resources/views/email/reservation.blade.php
@@ -10,5 +10,5 @@
     Comment: {{$data['comment']}}
 
     Hvala na ukazanom poverenju,
-    Tattoo
+    DadoTattoo
 @endcomponent
--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -21,7 +21,7 @@
 {{-- Footer --}}
 @slot('footer')
 @component('mail::footer')
-© {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
+© {{ date('Y') }} DadoTattoo. @lang('All rights reserved.')
 @endcomponent
 @endslot
 @endcomponent
